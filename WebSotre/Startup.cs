81fd5eac14d906f54269196aade4300233418c01
Cleber Dantas <cleberwd@gmail.com>
2014-10-15 19:43:02 -0300

    primeiro commit

---
+++ WebSotre/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebSotre.Startup))]
+namespace WebSotre
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
